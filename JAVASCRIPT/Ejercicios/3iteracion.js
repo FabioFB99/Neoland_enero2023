331696b78781b6ad1bf4ab186eda7436d79a28eb
FabioFB99 <fabiof1999@gmail.com>
2023-01-10 12:32:19 +0100

    1ejercicios

---
+++ JAVASCRIPT/Ejercicios/3iteracion.js
@@ -0,0 +1,32 @@
+/*
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+*/
+let resultado = 10 * 5;
+console.log(resultado);
+
+/*
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+*/
+resultado = 10 / 2; 
+console.log(resultado);
+/*
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+*/
+resultado = 15 % 9; 
+console.log(resultado);
+/*
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+*/
+const p = 10, j = 5;
+let pj = p + j;
+
+ if(o==pj) console.log("es correcta la afirmacion")
+/*
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.
+*/
+const i = 50;
+const c = 10, m = 5;
+let cm = c * m;
+    if(i==cm) console.log("es correcta la afirmacion")
